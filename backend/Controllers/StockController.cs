9462cd6534aafe340157a0946e94e966af2ea50f
DAYAN MANRIQUE <dayanalexismanrique@hotmail.com>
2024-04-24 20:16:49 -0500

    delete not using import

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using backend.Data;
 using backend.Dtos.Stock;
 using backend.Interfaces;
 using backend.Mappers;
-using backend.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
